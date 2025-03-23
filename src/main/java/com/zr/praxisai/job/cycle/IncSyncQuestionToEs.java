24b16558fd22902a4be555525997e34e2a6e985a
Echo211531 <www.2115310918@qq.com>
2025-03-23 22:21:39 +0800

    题目：全量同步+增量同步

--- src/main/java/com/zr/praxisai/job/cycle/IncSyncQuestionToEs.java
+++ src/main/java/com/zr/praxisai/job/cycle/IncSyncQuestionToEs.java
@@ -23,29 +23,29 @@ public class IncSyncQuestionToEs {
 
     @Resource
     private QuestionMapper questionMapper;
-
     @Resource
     private QuestionEsDao questionEsDao;
 
-    /**
-     * 每分钟执行一次
-     */
+    //每分钟执行一次
     @Scheduled(fixedRate = 60 * 1000)
     public void run() {
-        // 查询近 5 分钟内的数据
+        // 查询最近 5 分钟内的数据
         long FIVE_MINUTES = 5 * 60 * 1000L;
         Date fiveMinutesAgoDate = new Date(new Date().getTime() - FIVE_MINUTES);
+        // 查询最近 5 分钟内的所有数据列表
         List<Question> questionList = questionMapper.listQuestionWithDelete(fiveMinutesAgoDate);
         if (CollUtil.isEmpty(questionList)) {
             log.info("no inc question");
             return;
         }
+        //转换成ES请求实体类列表
         List<QuestionEsDTO> questionEsDTOList = questionList.stream()
                 .map(QuestionEsDTO::objToDto)
                 .collect(Collectors.toList());
         final int pageSize = 500;
         int total = questionEsDTOList.size();
         log.info("增量同步开始, 总计 {}", total);
+        //同理增量同步到ES中
         for (int i = 0; i < total; i += pageSize) {
             int end = Math.min(i + pageSize, total);
             log.info("sync from {} to {}", i, end);
--- src/main/java/com/zr/praxisai/job/once/FullSyncQuestionToEs.java
+++ src/main/java/com/zr/praxisai/job/once/FullSyncQuestionToEs.java
@@ -21,29 +21,29 @@ public class FullSyncQuestionToEs implements CommandLineRunner {
 
     @Resource
     private QuestionService questionService;
-
     @Resource
     private QuestionEsDao questionEsDao;
-
+    //实现 CommandLineRunner 接口后，Spring Boot 在应用启动时会自动调用 run 方法
     @Override
     public void run(String... args) {
-        // 全量从数据库中获取题目（数据量不大的情况下使用）
+        // 全量从数据库中获取所有题目（数据量不大的情况下使用）
         List<Question> questionList = questionService.list();
         if (CollUtil.isEmpty(questionList)) {
             return;
         }
-        // 转为 ES 实体类
+        // 转为 ES 请求实体类列表
         List<QuestionEsDTO> questionEsDTOList = questionList.stream()
                 .map(QuestionEsDTO::objToDto)
                 .collect(Collectors.toList());
         // 分页批量插入到 ES
-        final int pageSize = 500;
-        int total = questionEsDTOList.size();
+        final int pageSize = 500;  //定义每批次处理的数据量为 500 条
+        int total = questionEsDTOList.size(); //题目总数
         log.info("全量同步开始, 总计 {}", total);
         for (int i = 0; i < total; i += pageSize) {
             // 注意同步的数据下标不能超过总数据量
-            int end = Math.min(i + pageSize, total);
+            int end = Math.min(i + pageSize, total);  // 保证下标不越界
             log.info("sync from {} to {}", i, end);
+            //每次循环保存 [i, end) 数据到es中
             questionEsDao.saveAll(questionEsDTOList.subList(i, end));
         }
         log.info("全量同步结束, 总计 {}", total);
